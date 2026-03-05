ed784f26c407ed04057c9b59a23e91cdd999ac61
Aswin Sreeraj <aswinsreeraj96@gmail.com>
2026-03-05 23:17:55 +0530

    feat(router): protect profile route using ProtectedRoute

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,7 +1,10 @@
-import Layout from "../shared/components/Layout";
+import Layout from "../shared/Layout";
 import HomePage from "../modules/auth/pages/HomePage";
 import LoginPage from "../modules/auth/pages/LoginPage";
 import VerifyOtpPage from "../modules/auth/pages/VerifyOtpPage";
+import ProfilePage from "../modules/user/pages/ProfilePage";
+import ProtectedRoute from "../shared/components/ProtectedRoute";
+import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +13,13 @@ export const router = createBrowserRouter([
             { path: "/", element: <HomePage /> },
             { path: "/login", element: <LoginPage /> },
             { path: "/verify-otp", element: <VerifyOtpPage /> },
+            {   path: "/profile",
+                element: (
+                    <ProtectedRoute>
+                        <ProfilePage />
+                    </ProtectedRoute>
+                ),  
+            },
         ],
     },
 ]);
